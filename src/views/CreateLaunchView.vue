90f853e5e3c89f04e30fb3cf0c249c7c886e1b42
Allison Hamelin <allison.hamelin@veritree.com>
2025-03-30 22:56:25 -0700

    Fix navbar import

--- src/views/CreateLaunchView.vue
+++ src/views/CreateLaunchView.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import LayoutNavbar from '@/components/LayoutNavbar.vue'
+import LayoutNavbar from '@/components/layout/LayoutNavbar.vue'
 
 export default defineComponent({
   components: {
